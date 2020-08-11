a97763bd4b607708447a1f6622a871728d340651
Bastien Malgouyres <bmalgouyres@free.fr>
2020-08-11 05:15:21 +0200

    fuck %w()

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :reservation
 
-  validates :note, presence: true, inclusion: { in: %w(0..5),
-  message: "%{value} is not a valid note" }
+  validates :note, presence: true, numericality: { only_integer: true },
+  inclusion: { in: 0..5 , message: "It is not a valid note" }
   validates :content, presence: true
 end
