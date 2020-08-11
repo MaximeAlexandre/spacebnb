214aaa6e93589b26fb38dc6e18b7e4e70e6b163c
Maxime Alexandre <alexandre.maxime666@gmail.com>
2020-08-11 16:08:34 +0200

    seed ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+puts 'Creating 5 fake users with 2 fakes planet for each user...'
+5.times do
+  user = User.new(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email(name: :username),
+    password: Faker::Internet.password(min_length: 8),
+    address: Faker::Space.galaxy
+  )
+  user.save!
+
+  2.times do
+    planet = Planet.new(
+      name: Faker::Space.planet,
+      description: Faker::Space.constellation,
+      address: Faker::Space.galaxy,
+      price: rand(1000..5000),
+      user_id: user.id,
+      start_date: Date.today,
+      end_date: Date.today + rand(5..20)
+    )
+    planet.save!
+  end
+end
+
+puts 'Finished!'
+
+puts 'Creating 1 fake reservation...'
+  reservation = Reservation.new(
+    status: "pending",
+    start_date: Date.today,
+    end_date: Date.today + 5,
+    user_id: User.all.sample.id,
+    planet_id: Planet.all.sample.id
+  )
+  reservation.save!
+puts 'Finished!'
+
+puts 'Creating 1 fake review...'
+  review = Review.new(
+    note: 4,
+    content: "Sympa",
+    user_id: reservation.user_id,
+    reservation_id: reservation.id
+  )
+  review.save!
+puts 'Finished!'
