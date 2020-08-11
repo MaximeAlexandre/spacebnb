3204eaab4bbe92d4118eaaf145a8dbec6d365ef1
Maxime Alexandre <alexandre.maxime666@gmail.com>
2020-08-11 14:07:10 +0200

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :planets do
+    resources :reservations, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reservations, only: [:show]
+  resources :dashboards, only: [:index]
 end
