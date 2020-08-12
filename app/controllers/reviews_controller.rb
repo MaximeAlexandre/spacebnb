5b7ce37fa10843a8d042f5bba924a5b86f194466
Maxime Alexandre <alexandre.maxime666@gmail.com>
2020-08-12 11:32:04 +0200

    reviews controller and view new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @reservation = reservation.find(@review.reservation_id)
+    if @review.save
+      redirect_to reservation_path(@reservation)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:note, :content)
+  end
 end
