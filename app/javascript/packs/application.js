044b2df035bdd4ba3c5a40a98719e68dc1a6e9c6
Maxime Alexandre <alexandre.maxime666@gmail.com>
2020-08-12 15:09:42 +0200

    flatpickr bug resolved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import flatpickr from "flatpickr";
 
 // Internal imports, e.g:
 import { initFlatpickr } from '../plugins/init_flatpickr';
