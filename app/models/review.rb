4dfe82a8d1d40a90befcbf6e7c68627c2adecbb0
Maxime Alexandre <44815512+MaximeAlexandre@users.noreply.github.com>
2020-08-12 11:53:47 +0200

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :reservation
 
   validates :note, presence: true, numericality: { only_integer: true },
-  inclusion: { in: 0..5 , message: "It is not a valid note" }
+  inclusion: { in: 0..5, message: "It is not a valid note" }
   validates :content, presence: true
 end
